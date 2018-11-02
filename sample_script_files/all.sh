d51b4abc939b7195cd8624ae3226edffcf30ad64
pencurry <44125231+pencurry@users.noreply.github.com>
2018-11-02 17:48:42 +0700

    Update and rename run_OpenCL.sh to all.sh

--- sample_script_files/all.sh
+++ sample_script_files/all.sh
@@ -5,8 +5,8 @@
 worker=""
 pool="http://mine.arionumpool.com"
 wallet="42hL7nV8mZzgBWREmE2J4bdnpek3ckDAMKnomgtZga16LW611PksJewcj2fz89HLgh2MWa3L5PvmfpYt4innmShJ"
-threads="2"
-batches="24"
+threads="20"
+batches="48"
 
 # set this to false if you do not want miner to auto relaunch after crash
 relaunch_miner_on_crash="true"
@@ -14,7 +14,7 @@ relaunch_miner_on_crash="true"
 while :
 do
 	# -u means use all device, you can also use -d to specify list of devices (ex: -d 0,2,5)
-	./arionum-opencl-miner -u -b "$batches" -t "$threads" -p "$pool" -a "$wallet" -i "$worker" --skip-cpu-blocks
+	./arionum-cuda-miner -u -b "$batches" -t "$threads" -p "$pool" -a "$wallet" -i "$worker"
 
 	if [ "$relaunch_miner_on_crash" = "true" ]; then
 		echo "miner crashed, relaunching in 5 seconds ..."
