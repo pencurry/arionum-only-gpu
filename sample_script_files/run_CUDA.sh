0f22197f38f4a0584303016d28cb9dbef9191632
pencurry <44125231+pencurry@users.noreply.github.com>
2019-01-18 08:06:09 +0700

    Update run_CUDA.sh

--- sample_script_files/run_CUDA.sh
+++ sample_script_files/run_CUDA.sh
@@ -4,7 +4,7 @@
 # adjust -b & -t value as described in the README and FAQ
 worker=""
 pool="http://mine.arionumpool.com"
-wallet="42hL7nV8mZzgBWREmE2J4bdnpek3ckDAMKnomgtZga16LW611PksJewcj2fz89HLgh2MWa3L5PvmfpYt4innmShJ"
+wallet="2qFmhU3pHSJddgHYx9Bg5QEdmXHpiN84GpRYzAzpwj8fWvCsZWNoQS6sZiy5mH8VASomkUzrcKG7iGRj94fnFW7k"
 threads="1"
 batches="48"
 
